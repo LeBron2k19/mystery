2872baaa73b0f88d57f004485a36b38f4e671839
LeBron2k19 <LeBron2k19@users.noreply.github.com>
2019-02-26 11:33:53 -0400

    Added 3 Sorts

--- mystery.cpp
+++ mystery.cpp
@@ -10,22 +10,80 @@ void print(auto A)
    cout<<endl;
 }
 
-void mystery1(auto& Data)
-{
-  cout<<endl<<"Mystery 1"<<endl<<"---------------------"<<endl;
 
-  for ( int i = 0 ; i < Data.size( ) ; i++)
+void mystery1(auto& Data) //Selection Sort
+{
+  cout<<endl<<"Mystery 1 (Selection Sort)"<<endl<<"---------------------"<<endl;
+
+  unsigned int i, j, minIndex;
+  int tmp;
+  for (i = 0; i < Data.size() - 1; i++)
   {
-    for ( int j = 0 ; j < i ; j++)
+	minIndex = i;
+
+	for (j = i + 1; j < Data.size(); j++)
+	{
+		if (Data[j] < Data[minIndex])
+			minIndex = j;
+	}
+
+	if (minIndex != i)
+	{
+		swap(Data[i], Data[minIndex]);
+		
+		/*	 tmp = Data[i]; 		//Alternative Swap
+			 Data[i] = Data [minIndex];
+			 Data[minIndex] = tmp; 		*/   
+	}
+	
+	print(Data);
+
+  } //End outer for-loop
+}   // End function
+
+
+
+
+void mystery2(auto& Data) //Bubble Sort
+{
+  cout<<endl<<"Mystery 2 (Bubble Sort)"<<endl<<"---------------------"<<endl;
+
+  for (unsigned int i = 0 ; i < Data.size( ) ; i++)
+  {
+    for (unsigned int j = 0 ; j < i ; j++)
 	if ( Data[ i ] < Data[ j ] )
 	    swap( Data[ i ] , Data[ j ] );
 
     print(Data);
-  }//end outer for (this brace is needed to include the print statement)
+  }		//Ends outer for-loop (this brace is needed to include the print 			statement, {which prints after each iteration})
 
 }
 
-//... Other mysteries...
+
+void mystery3(auto& Data) //Insertion Sort
+{
+  cout<<endl<<"Mystery 3 (Insertion Sort)"<<endl<<"---------------------"<<endl;
+
+int nextIndex, moveitem, insertVal;
+
+  for (nextIndex = 1; nextIndex < Data.size(); nextIndex ++)
+  {
+	insertVal = Data[nextIndex];
+	moveitem = nextIndex;
+
+	while (moveitem > 0 && Data[moveitem -1] > insertVal)
+	{
+		Data[moveitem]  = Data[moveitem -1];
+		moveitem--;
+	} //End While
+	
+	Data[moveitem] = insertVal;
+
+	print(Data);
+  }	//End For
+}  	//End Function
+
+
 
 int main()
 {
